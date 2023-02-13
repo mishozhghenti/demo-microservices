85870999aaa6c5cac94fc6de07f236fe8d565703
misho <mishozhghenti@gmail.com>
2023-02-13 01:18:04 +0100

    Create order-service module

---
+++ order-service/src/main/java/com/flatrock/orderservice/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.flatrock.orderservice.repository;
+
+import com.flatrock.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ order-service/src/main/java/com/flatrock/orderservice/service/OrderService.java
@@ -0,0 +1,47 @@
+package com.flatrock.orderservice.service;
+
+import com.flatrock.orderservice.dto.OrderLineItemsDto;
+import com.flatrock.orderservice.dto.OrderRequest;
+import com.flatrock.orderservice.model.Order;
+import com.flatrock.orderservice.model.OrderLineItems;
+import com.flatrock.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
